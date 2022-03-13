f80c11bdbef37b504d33075c4f9371c77eaefc72
rikonnect <chachaerickjo@gmail.com>
2022-03-13 16:56:38 +0300

    rf: change dummy text in description to actual readable text

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let card = document.querySelectorAll(".card")
+let image = document.querySelectorAll(".hideShow")
+let text = document.querySelectorAll(".showHide")
+card[0].addEventListener("click",(e)=>{
+    e.preventDefault()
+    if(text[0].style.display == "none"){
+        text[0].style.display = "block"
+        image[0].style.display="none"
+    }
+    else{
+        image[0].style.display = "block"
+        text[0].style.display = "none"
+    }
+})
+card[1].addEventListener("click",(e)=>{
+    e.preventDefault()
+    if(text[1].style.display == "none"){
+        text[1].style.display = "block"
+        image[1].style.display="none"
+    }
+    else{
+        image[1].style.display = "block"
+        text[1].style.display = "none"
+    }
+})
+card[2].addEventListener("click",(e)=>{
+    e.preventDefault()
+    if(text[2].style.display == "none"){
+        text[2].style.display = "block"
+        image[2].style.display="none"
+    }
+    else{
+        image[2].style.display = "block"
+        text[2].style.display = "none"
+    }
+})
